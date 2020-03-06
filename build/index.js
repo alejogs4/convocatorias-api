617a9a9310988aed0d62816d4051f50348e962ae
alejogs4 <alejogs4@gmail.com>
2020-03-06 14:20:26 +0000

    Add new header

--- build/index.js
+++ build/index.js
@@ -5,6 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 // scripts
 var app_1 = __importDefault(require("./app"));
-app_1.default.listen(3000, function () {
-    console.log("Server running on http://localhost:3000");
+app_1.default.listen(process.env.PORT, function () {
+    console.log("Server running on http://localhost:" + process.env.PORT);
 });
--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ const app: Express = express();
 function access(_: Request, res: Response, next: NextFunction): void {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, authorization');
   next();
 }
 
