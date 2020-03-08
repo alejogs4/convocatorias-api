139a5a6f64a3475f62966913a77a7eaf0fcbfc2c
alejogs4 <alejogs4@gmail.com>
2020-03-08 14:37:12 -0500

    Change get jobs queries

--- src/api/business/jobs/jobs.sql.ts
+++ src/api/business/jobs/jobs.sql.ts
@@ -1,5 +1,5 @@
 const JOBS_SQL_QUERIES = {
-  getJobsOpportunities: `SELECT name, description, begin_date, final_date, is_close, open_teacher_id, job_type_id FROM jobs WHERE is_close=false`,
+  getJobsOpportunities: `SELECT id, name, description, begin_date, final_date, is_close, open_teacher_id, job_type_id FROM jobs WHERE is_close=false ORDER BY id DESC`,
   createNewJobOpportunity: `
   INSERT INTO jobs(name, description, begin_date, final_date, open_teacher_id, job_type_id)
   VALUES($1, $2, $3, $4, $5, $6) RETURNING *
